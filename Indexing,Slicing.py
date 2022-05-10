41dabe33d102de9f5ce53935e8d17c50dc357003
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-05-10 09:04:55 -0500

    Update Indexing,Slicing.py

--- Indexing,Slicing.py
+++ Indexing,Slicing.py
@@ -1,5 +1,26 @@
+word = "python"
+print(word[2:4])        #this will print th, which is characters from position 0 (included) to 4(excluded)
+
+---
+
+word = "python"
+print(word[2:])         #this will print characters from position 2(included) to the end
+
+---
+
+word = "python"
+print(word[:2])         #this will print characters from the beginning position to 2(excluded)
+
+---
+
+#this also makes sure that word[:2] + word[2:] is always equal to word
+word = "python"
+print(word[:2]+word[2:])  #python
+
+---
+
 a = "1234567Rainy"
-print(a[1:-1:2]) #이상:미만:간격
+print(a[1:-1:2]) #[1(included position):-1(excluded position):2(interval)]
 
 result : 
 '''
