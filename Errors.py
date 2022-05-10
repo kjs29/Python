f0d5f19788b2d248e4b00429d759a7d294700a47
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-05-10 07:59:29 -0500

    Create Errors.py

---
+++ Errors.py
@@ -0,0 +1,11 @@
+'''
+There are two types of erros
+1.SyntaxError
+2.NameError
+
+SyntaxError is about grammar.
+NameError is all the rest of errors other than SyntaxError
+'''
+
+print("Hello')      #this will produce SyntaxError
+print(Greeting)     #this will produce NameError
