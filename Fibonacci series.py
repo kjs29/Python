e39085616ec381dfd136f166e4d34738d5b25852
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-05-10 12:37:17 -0500

    Update Fibonacci series.py

--- Fibonacci series.py
+++ Fibonacci series.py
@@ -1,12 +1,8 @@
-a = 0
-b = 1
-while a < 10
-  print(a)
-  a = b
-  b = a + b
 
-  
-  
+a,b = 0,1
+while a < 10 : 
+  print(a)
+  a,b = b,a+b  
   
 '''
 First sequence
@@ -14,6 +10,8 @@ a = 0
 b = 1
 a<10 is true
 prints a which is 0
+
+The next two lines happens simultaneously
 a becomes b which is 1
 b becomes a+b which is 0 + 1 , not 1 + 1. a still has value of 0, we should add original value of a.
 
@@ -64,4 +62,61 @@ result :
 3
 5
 8
+'''
+#Difference between multiple assignments and regular assignments.
+#Multiple assignments simultaneously assign values, but regular assignment is affected by chronological order
+
+#let's take a look at the example using regular assignments.
+a = 0
+b = 1
+while a<10:
+  print(a)
+  a = b
+  b = a+b
+'''
+First sequence
+a = 0
+b = 1
+a < 10 is true
+prints a which is 0
+a becomes b which is 1
+b becomes a + b which is 1 + 1 = 2
+
+Second sequence
+a = 1
+b = 2
+a<10 is true
+prints a which is 1
+a becomes b which is 2
+b becomes a + b which is 2 + 2 = 4
+
+Third sequence
+a = 2
+b = 4
+a < 10 is true
+prints a which is 2
+a becomes b which is 4
+b becomes a + b which is 4 + 4 = 8
+
+Forth sequence
+a = 4
+b = 8
+a < 10 is true
+prints a which is 4
+a becomes b which is 8
+b becomes a + b which is 8+8 = 16
+
+Fifth sequence
+a = 8
+b = 16
+a < 10 is true
+prints a which is 8
+a becomes b which is 16
+b becomes a + b which is 16 + 16 = 32
+
+Sixth sequence
+a = 16
+b = 32
+a < 10 is NOT true
+break out of while loop.
 '''
