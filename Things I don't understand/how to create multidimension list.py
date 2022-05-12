8712eaedb406330a4222021c9f38d7855f99d22f
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-05-12 11:18:27 -0500

    Create how to create multidimension list.py

---
+++ Things I don't understand/how to create multidimension list.py
@@ -0,0 +1,10 @@
+"""
+I created two different lists
+And I want to merge them together
+and I want to have a new list called gradebook that looks like
+gradebook = [["physics",98],["calculus",97],["poetry",85],["history",88]]
+how can I do that without manually creating one?
+"""
+
+subjects = ["physics","calculus","poetry","history"]
+grades = [98,97,85,88]
