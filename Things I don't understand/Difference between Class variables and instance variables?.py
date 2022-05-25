b9b6cd66d0c84b15a442d7d65a39c629786cd98b
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-05-24 23:17:59 -0500

    Update Difference between Class variables and instance variables?.py

--- Things I don't understand/Difference between Class variables and instance variables?.py
+++ Things I don't understand/Difference between Class variables and instance variables?.py
@@ -3,6 +3,18 @@ What is the difference between Class variables and Instance Variables?
 
 """
 
-class Person:
+class Employee:
   def __init__(self, name):
-    self.name = name      #when should I use self.name instead of Person.name?
+    self.name = name      #when should I use self.name instead of Employee.name?
+
+    
+    
+    
+I guess class variables apply to all instances
+and instance variables only apply to that specific instance
+
+When to use Class Variables vs When to use Instance Variables
+
+If we want to create some attributes that can apply individual instance, that is changeable -> we use self.raise_amount
+If we want to use the variables that apply to all instances in a class, -> we use Employee.number_of_emps
+
