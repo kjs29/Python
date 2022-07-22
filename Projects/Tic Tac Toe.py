9fb0f113287bc482bee50b40b0728b72f1fc771e
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-07-22 07:14:23 -0500

    Create Tic Tac Toe.py

---
+++ Projects/Tic Tac Toe.py
@@ -0,0 +1,31 @@
+from practice2 import check_for_win, check_turn, draw_board
+import os
+
+spots = {1:"1", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9",}
+turn = 0
+playing = True
+
+while playing:
+    #os.system("cls" if os.name == "nt" else "clear")
+    print("\nboard : ")
+    draw_board(spots)
+    print(f"< Turn {turn + 1} >")
+    print(f"Player {(turn%2)+ 1}'s turn \nplayer 1: X\nplayer 2: O")
+    choice = input("Enter your number or press 'q' to quit : ")
+    if choice == "q":
+        playing = False
+    elif choice.isdigit() and int(choice) in spots:
+        if spots[int(choice)] not in "XO":
+            turn += 1
+            spots[int(choice)] = check_turn(turn)
+        else:
+            print("Invalid input, please try again :)")
+    if check_for_win(spots):
+        print(f"\t      Player {(turn%2)} win")
+        draw_board(spots)
+        break
+    elif turn >= 8 and check_for_win(spots) == False:
+        print("\t      Tied!")
+        draw_board(spots)
+        break
+print("Thank you for playing")
