9f0698bb9da8b86117e486abeef0c5acab6ddcb7
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-07-22 07:15:01 -0500

    Update main.py

--- Projects/Tic Tac Toe/main.py
+++ Projects/Tic Tac Toe/main.py
@@ -1,4 +1,4 @@
-from practice2 import check_for_win, check_turn, draw_board
+from help import check_for_win, check_turn, draw_board
 import os
 
 spots = {1:"1", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9",}
