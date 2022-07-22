370afe831b0c394ead3d87c664135abe6c0d239c
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-07-22 07:15:19 -0500

    Create help.py

---
+++ Projects/Tic Tac Toe/help.py
@@ -0,0 +1,22 @@
+def draw_board(spot):
+    board = f"\t\t|{spot[1]}|{spot[2]}|{spot[3]}|\n\t\t|{spot[4]}|{spot[5]}|{spot[6]}|\n\t\t|{spot[7]}|{spot[8]}|{spot[9]}|"
+    print(board)
+
+def check_turn(turn):
+    if turn % 2 == 0: return "O"
+    else: return "X"
+
+def check_for_win(spot):
+    if     (spot[1] == spot[2] == spot[3]) \
+        or (spot[4] == spot[5] == spot[6]) \
+        or (spot[7] == spot[8] == spot[9]) :
+        return True
+    elif   (spot[1] == spot[4] == spot[7]) \
+        or (spot[2] == spot[5] == spot[8]) \
+        or (spot[3] == spot[6] == spot[9]) :
+        return True
+    elif   (spot[1] == spot[5] == spot[9]) \
+        or (spot[3] == spot[5] == spot[7]) :
+        return True
+    else:
+        return False
