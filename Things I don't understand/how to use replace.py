79578646b644fce82339e65d22982d3bca78eb54
Jinsung Kim <96529477+kjs29@users.noreply.github.com>
2023-03-28 23:20:13 -0600

    Update how to use replace.py

--- Things I don't understand/how to use replace.py
+++ Things I don't understand/how to use replace.py
@@ -3,3 +3,7 @@
 garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
 garden_waitlist.replace("Adam", "Calla")
 print("garden_waitlist")
+
+
+
+#Solved: Because it is a list not a string. .replace works with string.
